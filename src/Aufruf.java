c77d4b547eafd580d697bfa2a5f0b3e23920f03f
Liscak Adam <1810653053@stud.fh-kufstein.ac.at>
2018-12-05 12:23:03 +0100

    Aufgabe5_2

--- src/Aufruf.java
+++ src/Aufruf.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
 
 public class Aufruf {
+    public static void main(String[] args)
+    {
+  Vehicle ladaniva = new Vehicle();
+  ladaniva.setPower(30);
+  ladaniva.setName("VAZ 1204");
+  ladaniva.setWeight(900.34);
+  ladaniva.calculatePower();
+  Vehicle daewoo = new Vehicle() ;
+  daewoo.setPower(27);
+  daewoo.setName("Daewoo Accent");
+  daewoo.setWeight(834.3);
+  daewoo.calculatePower();
+        System.out.println(ladaniva.printName());
+        System.out.println(daewoo.printName());
+    }
 }
--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+import javax.swing.*;
 
-public class Vehicle {
+public class Vehicle
+{
+    private double weight;
+    private int power;
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String printName()
+    {
+        return name;
+    }
+    public void setPower(int power) {
+        this.power = power;
+    }
+    public int getPower() {
+        return power;
+    }
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+    public double getWeight() {
+        return weight;
+    }
+
+    public void calculatePower()
+    {
+        JOptionPane.showMessageDialog(null, "Die Kraft in PS für "+name+"="+1.36*power);
+    }
 }
